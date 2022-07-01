0af27c43a07b79872626919af02095c888fe399b
Caro Appleby <caroline.appleby@cantab.net>
2022-07-01 16:38:05 +0100

    Fix build command?

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        ...(process.env.JEKYLL_ENV == "production"
+            ? [require("cssnano")({ preset: "default" })]
+            : []),
     ]
 }
